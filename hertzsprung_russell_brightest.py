ff77f8f5a0083948e10cc5fcd7963dd196a017d2
Lena Messerschmidt <messerschmidt.lena@gmail.com>
2021-01-20 15:05:23 +0100

    Add task 4

---
+++ hertzsprung_russell_brightest.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+x = []
+y = []
+
+with open('table_brightest_stars.csv','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    next(plots)
+    for row in plots:
+        if (len(row[5]) == 0) or (len(row[3]) == 0):
+            continue
+        else:
+            x.append(float(row[5]))
+            y.append(float(row[3]))
+
+
+
+fig, ax = plt.subplots()
+
+ax.plot(x,y, '.',)
+ax.set_xlabel('color index B - V')
+ax.set_ylabel('absolute Mag M_V')
+
+plt.title('Brightest Stars')
+plt.xticks(np.arange(-0.3, 2, 0.2))
+plt.yticks(np.arange(-10, 20, 5))
+
+plt.show()
+
